5fb67bf8bff42bb5653531e1ba44b5746356e1da
William Metcalf <metcalf_william@network.lilly.com>
2021-02-08 16:42:45 -0500

    setup? still???

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-import { Box, styled, Typography } from "@material-ui/core";
+import { AppBar, Box, styled, Toolbar, Typography } from "@material-ui/core";
 
 interface Props {
   style?: React.SVGAttributes<SVGSVGElement>["style"];
@@ -10,37 +10,43 @@ const Header: FC<Props> = (props) => {
   const { style } = props;
 
   return (
-    <StyledBox display="flex">
-      <svg
-        width={85}
-        height={100}
-        viewBox="0 0 85 100"
-        preserveAspectRatio="none"
-        style={{ stroke: "gray", ...style }}
-      >
-        <path
-          d={`M 10 0 l -10 100 l 20 -50 l 5 50 l 20 -100`}
-          fill="transparent"
-          strokeWidth={3}
-        />
-        <path
-          d={`M 30 100 l 20 -100 l 5 50 l 25 -50 l -20 100`}
-          fill="transparent"
-          strokeWidth={3}
-        />
-      </svg>
-      <Box flex={1} />
-      <Typography>About</Typography>
-      <Typography>Work</Typography>
-      <Typography>Services</Typography>
-      <Typography>Blog</Typography>
-      <Typography>News</Typography>
-      <Typography>Contact</Typography>
-    </StyledBox>
+    <AppBar style={{ backgroundColor: "transparent" }}>
+      <StyledToolbar>
+        <svg
+          width={42}
+          height={50}
+          viewBox="0 0 85 100"
+          preserveAspectRatio="none"
+          style={{
+            stroke: "gray",
+            strokeWidth: 3,
+            fill: "transparent",
+            ...style,
+          }}
+        >
+          <path
+            d={`M 10 0 l -10 100 l 20 -50 l 5 50 l 20 -100`}
+            fill="transparent"
+          />
+          <path
+            d={`M 30 100 l 20 -100 l 5 50 l 25 -50 l -20 100`}
+            fill="transparent"
+          />
+        </svg>
+        <Box flex={0.5} />
+        <Typography>About</Typography>
+        <Typography>Work</Typography>
+        <Typography>Services</Typography>
+        <Typography>Blog</Typography>
+        <Typography>News</Typography>
+        <Typography>Contact</Typography>
+      </StyledToolbar>
+    </AppBar>
   );
 };
 
-const StyledBox = styled(Box)({
+const StyledToolbar = styled(Toolbar)({
+  justifyContent: "center",
   "&>*": {
     marginRight: "3em",
   },
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import "../config/style.css";
 
 import React, { FC } from "react";
-import { Parallax } from "react-parallax";
 
 import { Box, styled } from "@material-ui/core";
 
@@ -11,12 +10,9 @@ const HomePage: FC = () => {
   return (
     <Box style={{ minHeight: "100vh" }}>
       <Header />
-      <Parallax blur={{ min: -15, max: 15 }} strength={-200}>
-        <Section style={{ backgroundColor: "red" }}></Section>
-        <Section style={{ backgroundColor: "blue" }}></Section>
-        <Section style={{ backgroundColor: "green" }}></Section>
-        <Section style={{ backgroundColor: "yellow" }}></Section>
-      </Parallax>
+      <Section style={{ backgroundColor: "darkgray" }}></Section>
+      <Section style={{ backgroundColor: "gray" }}></Section>
+      <Section style={{ backgroundColor: "lightgray" }}></Section>
     </Box>
   );
 };
